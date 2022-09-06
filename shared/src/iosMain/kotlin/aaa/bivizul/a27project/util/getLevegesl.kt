27e6baab5ce9f7b4850f307fad234a09aa1a9cd2
Bivizul <mamochapa@gmail.com>
2022-09-06 12:10:12 +0200

    finish

---
+++ shared/src/iosMain/kotlin/aaa/bivizul/a27project/util/getLevegesl.kt
@@ -0,0 +1,34 @@
+package aaa.bivizul.a27project.util
+
+actual fun getLevegesl(): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getLevegesmm(): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getLevegessim(contextAny: Any): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getLevegesid(contextAny: Any): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getLevegest(): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getLevegedia(contextAny: Any, activityAny: Any) {
+}
+
+actual fun checkLevegenet(contextAny: Any): Boolean {
+    TODO("Not yet implemented")
+}
+
+actual fun levSignalOff() {
+}
+
+actual fun levscreenExit(activityAny: Any) {
+}
---
+++ shared/src/iosMain/kotlin/aaa/bivizul/a27project/widget/BackImage.kt
@@ -0,0 +1,8 @@
+package aaa.bivizul.a27project.ui.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+actual inline fun BackImage(modifier: Modifier) {
+}
